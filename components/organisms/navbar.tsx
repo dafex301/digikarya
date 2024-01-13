dda269d7f177e89f4291596f299b4d88f56cc23a
Fahrel <fahrel@students.undip.ac.id>
2024-01-13 17:06:59 +0700

    fix: navbar

--- components/organisms/navbar.tsx
+++ components/organisms/navbar.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from "../molecules/mode-toggle";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between flex-wrap p-6 sticky top-0 dark:bg-slate-950 z-10">
+    <nav className="flex items-center justify-between flex-wrap p-6 sticky top-0 dark:bg-slate-950 bg-white z-10">
       <Link
         href="/"
         className="font-semibold text-2xl tracking-tight text-main dark:text-sub"
